d059c744a70ecf375378710a4a440af9feb5e11f
1229341617 <1229341617@qq.com>
2019-10-22 15:38:08 +0800

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+zookeeper、kafka、nginx+lua脚本开发、storm、
+springboot、hystrix、线程池、hystrix+降级+线程池优化、
+RabitMQ、SpringCloud+SpringBoot+Mysql整合、Eureka+Zuul网关、Docker、jenkins
---
+++ ws/eshop-inventory/src/main/java/com/roncoo/eshop/inventory/App.java
@@ -0,0 +1,13 @@
+package com.roncoo.eshop.inventory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
